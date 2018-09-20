f27ac172c5665b84540a35d44e68f7accbdc1d38
Eirik Hemstad <eirikzh@gmail.com>
2018-09-20 12:19:52 +0200

    Add existing files

--- Hitra.java
+++ Hitra.java
@@ -15,9 +15,9 @@ import lejos.hardware.sensor.*;
 import lejos.hardware.Button;
 import java.util.*;
 
-public class Hitra
-{
-	// Globale variabler for aa kunne hente dem i kjor og setup
+public class Hitra {
+
+	// Globale E3-klasser
 	private static Brick brick;
 	private static Port s1;
 	private static Port s2;
@@ -35,179 +35,196 @@ public class Hitra
 	private static float[] trykkSample;
 	private static SampleProvider trykksensor2;
 	private static float[] trykkSample2;
-	private static int runde;
-	private static int svart;
 
+	// Andre globale variabler
+	private static int svart;
 	private static Random rand;
 
-	public static void main (String[] args)  throws Exception {
+	// Konstanter
+	private static final int SPEED = 450;
+	private static final int BRUSH_SPEED = 1800;
+	private static final int ANT_RUNDER = 8;
+	private static final double MAKS_LYD = 0.7;
 
+	public static void main(String[] args) {
 		// Oppsett av robotens komponenter
-		brick = BrickFinder.getDefault();
-		s1 = brick.getPort("S1"); // Lydsensor
-		s2 = brick.getPort("S2"); // Trykksensor 1 
-		s3 = brick.getPort("S3"); // Trykksensor 2
-		s4 = brick.getPort("S4"); // Fargesensor
-		ev3 = (EV3) BrickFinder.getLocal();
+		brick = BrickFinder.getDefault();		//Laster inn EV3-brikken
+
+		// Porter
+		s1 = brick.getPort("S1");		// Lydsensor
+		s2 = brick.getPort("S2");		// Trykksensor 1
+		s3 = brick.getPort("S3");		// Trykksensor 2
+		s4 = brick.getPort("S4");		// Fargesensor
+
+		ev3 = (EV3)BrickFinder.getLocal();
 		lcd = ev3.getTextLCD();
 		keys = ev3.getKeys();
+
 		lydSensor = new NXTSoundSensor(s1);
 		trykksensor = new EV3TouchSensor(s2);
 		trykksensor2 = new EV3TouchSensor(s3);
 		fargeSensor = new EV3ColorSensor(s4);
 
+		// Definer random
 		rand = new Random();
-		
-		// Antall ganger roboten skal krasje foer den stopper
-		runde = 2;
 
 		// Sett opp en definisjon av svart
+		definerSvart();
+
+		// Sett motorfart
+		Motor.A.setSpeed(SPEED);
+		Motor.B.setSpeed(SPEED);
+		Motor.C.setSpeed(BRUSH_SPEED);
+
+		for(int i = 0; i < ANT_RUNDER; i++) {
+			System.out.println("Runde " + i);
+			kjor();
+		}
+	}
+
+	private static void kjor() {
+		// Start motoren
+		startMotor(false);
+
+		boolean fortsett = true;
+
+		while (true) {
+			// TODO: Sjekk trykk
+			int retning = sjekkTrykk();
+			if (retning != 0) {
+				System.out.println("Traff hindring!");
+				startMotor(true);
+				vent(1000);
+				sving((rand.nextInt((600 - 100) + 1) + 100), retning);
+				break;
+			}
+
+			// TODO: Sjekk farge
+			if (skannSvart()) {
+				startMotor(false);
+				vent(1000);
+				sving(1080, 1);
+				break;
+			}
+			// TODO: Sjekk lyd
+			if (sjekkLyd()) {
+				stoppMotor(3000);
+				break;
+			}
+		}
+	}
+
+	// Svinger til et bestemt antall grader. Retning: 1 = venstre, -1 = høyre
+	private static void sving(int grader, int retning) {
+		if (retning < -1 || retning > 1)
+			throw new IllegalArgumentException("Retning må være definert som -1 eller 1.");
+
+		grader = grader * retning;
+		Motor.B.stop();
+		Motor.A.rotate(grader);
+		//while (Motor.A.isMoving()) Motor.B.rotate(-grader);
+	}
+
+	private static void startMotor(boolean revers) {
+		if (!revers) {
+			Motor.A.backward();
+			Motor.B.backward();
+		}
+		else {
+			Motor.A.forward();
+			Motor.B.forward();
+		}
+
+		Motor.C.forward();
+	}
+
+	private static void stoppMotor(int varighet) {
+		if (varighet < 0)
+			throw new IllegalArgumentException("Varighet kan ikke være i minus.");
+
+		Motor.A.stop();
+		Motor.B.stop();
+		Motor.C.stop();
+
+		try {
+			Thread.sleep(varighet);
+		}
+		catch(InterruptedException e) {
+		   	System.out.println("thread 2 interrupted");
+        }
+	}
+
+	// Returnerer 0 for ingen, 1 for venstre og -1 for høyre
+	private static int sjekkTrykk() {
+		trykkSample = new float[trykksensor.sampleSize()];
+		trykkSample2 = new float[trykksensor2.sampleSize()];
+
+		if (trykkSample2 != null && trykkSample2.length > 0){
+			trykksensor2.fetchSample(trykkSample2, 0);
+			if (trykkSample2[0] > 0){
+				return -1;
+			}
+		}
+		if (trykkSample != null && trykkSample.length > 0){
+			trykksensor.fetchSample(trykkSample, 0);
+			if (trykkSample[0] > 0){
+				return 1;
+			}
+		}
+
+		return 0;
+	}
+
+	private static void vent(int varighet) {
+		if (varighet < 0)
+			throw new IllegalArgumentException("Varighet kan ikke være minus.");
+
+		try {
+			Thread.sleep(varighet);
+		}
+		catch(InterruptedException e) {
+		   	System.out.println("thread 2 interrupted");
+        }
+	}
+
+	private static boolean sjekkLyd() {
+		lydSample = new float[lydSensor.sampleSize()];
+
+		if(lydSample != null && lydSample.length > 0) {
+			lydSensor.fetchSample(lydSample, 0);
+			if (lydSample[0] > MAKS_LYD) {
+				System.out.println("Lyd over " + MAKS_LYD + ": " + lydSample[0]);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	// Finner en definisjon for 'svart' farge
+	private static void definerSvart() {
 		fargeLeser = fargeSensor.getMode("RGB");
 		fargeSample = new float[fargeLeser.sampleSize()];
 		svart = 0;
+
 		for(int i = 0; i < 100; i++) {
 			fargeLeser.fetchSample(fargeSample, 0);
 			svart += fargeSample[0] * 100;
 		}
 
 		svart = svart / 100 + 5;
-
-		// Motorfart
-	 	Motor.A.setSpeed(900);
-	 	Motor.B.setSpeed(900);
-	 	Motor.C.setSpeed(450);
-	 	// Kjoer framover
-		kjor();
-	}
-
-	public static void kjor() {
-		lydSample = new float[lydSensor.sampleSize()];
-		trykkSample = new float[trykksensor.sampleSize()];
-		trykkSample2 = new float[trykksensor2.sampleSize()];
-
-		System.out.println("Kjorer til trykk");
-		Motor.A.backward();
-		Motor.B.backward();
-		Motor.C.forward();
-		boolean fortsett  = true;
-		int ret = 0;
-		boolean lydStopp = false;
-		boolean svartStopp = false;
-		boolean sensorSving = false;
-
-		while(fortsett) {
-			lydStopp = false;
-			sensorSving = false;
-			svartStopp = false;
-			// Dersom trykksensoren registrerer trykk, startes en prosess der bilen stoppes, rygger og svinger
-			if (trykkSample2 != null && trykkSample2.length > 0){
-				trykksensor2.fetchSample(trykkSample2, 0);
-				if (trykkSample2[0] > 0){
-					//System.out.println("Hindring2");
-					fortsett = false;
-					ret = 0;
-					lydStopp = false;
-				}
-			}
-			if (trykkSample != null && trykkSample.length > 0){
-				trykksensor.fetchSample(trykkSample, 0);
-				if (trykkSample[0] > 0){
-					//System.out.println("Hindring");
-					fortsett = false;
-					ret = 1;
-					lydStopp = false;
-				}
-			}
-			// Sjekker om fargeleseren returnerer svart farge
-			if (skannSvart()) {
-				fortsett = false;
-				svartStopp = true;
-			}
-			// Stopper ved en hoey lyd
-			if(lydSample != null && lydSample.length > 0) {
-				lydSensor.fetchSample(lydSample, 0);
-				if (lydSample[0] > 0.7) {
-					System.out.println("Lyd over 0.7: " + lydSample[0]);
-					fortsett = false;
-					lydStopp = true;
-				}
-			}
-		}
-		stopp();
-
-		// Kjoer metodene rygg og sving samtidig
-
-		if(svartStopp){
-			rygg();
-			ret =0;
-			int deg = 1300;
-			sving(ret, deg);
-			kjor();
-		}
-		else if (!lydStopp) {
-			rygg();
-			int deg = rand.nextInt((600 - 100) + 1) + 100;
-			sving(ret, deg);
-
-			runde--;
-			if(runde>0){
-		 		kjor();
-	 		}
-		}
-		else  {
-			try {
-				Thread.sleep(3000);
-			}
-			catch (InterruptedException e) {
-				System.out.println("En feil skjedde.");
-			}
-			kjor();
-		}
-	}
-
-	// Stopper begge hjulmotorene
-	public static void stopp(){
-		Motor.A.stop();
-		Motor.B.stop();
-		Motor.C.stop();
-	}
-
-	// Faar bilen til aa rygge
-	public static void rygg(){
-		Motor.A.forward();
-		Motor.B.forward();
-		try {
-			Thread.sleep(1000);
-		}
-		catch(InterruptedException e){
-		   	System.out.println("thread 2 interrupted");
-        }
 	}
 
 	public static boolean skannSvart() {
 		fargeLeser.fetchSample(fargeSample, 0);
-
-		if (fargeSample[0]*100 > 4) {
-			// TODO: Foelg svart linje
-			System.out.println("Ikke svart! (" + fargeSample[0]*100 + "/" + svart + ")");
+		
+		if (fargeSample[0]*100 > 5) {
+			// TODO: Følg svart linje
+			
 			return false;
 		} else {
-			System.out.println("Svart! (" + fargeSample[0]*100 + "/" + svart +")");
+			System.out.println("Fant svart linje!");
 			return true;
 		}
-	}
-
-	// Faar bilen til aa svinge. Dersom ret = 0, svinger bilen til venstre. Er ret = 1, hoeyre
-	public static void sving(int ret, int deg){
-		System.out.println("Svinger");
-		if (ret==1){
-			deg = deg*-1;
-			Motor.B.forward();
-		}
-		else {
-			Motor.B.backward();
-		}
-		Motor.A.rotate(deg);
-		while (Motor.A.isMoving()) Thread.yield();
 	}
 }
